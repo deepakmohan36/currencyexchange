fa7aa8b95bb0cc74be6fda4fec1b38b5cb56b895
dataguydeepak <deepak_mohan@condenast.com>
2024-10-14 13:39:31 +0530

    add requirements.txt file to extract folder

--- src/extract/main.py
+++ src/extract/main.py
@@ -10,21 +10,13 @@ def main(request):
     try:  
 
         currency_keys_json = currency_mapping.currency_key_mapping()
-
-        #extract the list of currency keys
         currency_keys_list = list(currency_keys_json.keys())
-
-        #extract converstion rates by passing the list
         daily_conversion_rates = conversion_data.daily_conversion_data(currency_keys_list)
-
-        #load the result to GCS
         load_to_gcs.load_to_gcs(daily_conversion_rates)
-
 
         return {
             "result": "success"
         }, 200
-    
     
     except Exception as e:
         return {
